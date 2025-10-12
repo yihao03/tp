408d35a994e49369943f7f679372e5e0a19d2f89
yihao <yihao039@gmail.com>
2025-10-12 19:17:04 +0800

    chore: add Javadoc comments

--- src/main/java/seedu/address/model/person/Student.java
+++ src/main/java/seedu/address/model/person/Student.java
@@ -5,7 +5,19 @@ import java.util.Set;
 
 import seedu.address.model.tag.Tag;
 
+/**
+ * Represents a Student in the address book. A Student is a {@link Person} that
+ * can be associated with one or more {@link Parent} instances.
+ * <p>
+ * Note: Parents are stored in insertion order. This class does not prevent
+ * duplicates. Initially, a newly constructed Student has no associated parents.
+ */
 public class Student extends Person {
+
+    /**
+     * Parents associated with this student. This list is internal to the model
+     * and may contain duplicates.
+     */
     private ArrayList<Parent> parents = new ArrayList<>();
 
     /**
@@ -26,6 +38,9 @@ public class Student extends Person {
      * Adds the given parent to this student and establishes a bidirectional
      * relationship. Appends the parent to this student's list of parents and
      * invokes {@code parent.addChild(this)}.
+     * <p>
+     * No deduplication is performed; if the parent is already associated, it
+     * will be added again.
      *
      * @param parent the parent to associate with this student; must not be null
      */
@@ -34,6 +49,11 @@ public class Student extends Person {
         parent.addChild(this);
     }
 
+    /**
+     * Returns this person's type.
+     *
+     * @return {@link PersonType#STUDENT}
+     */
     @Override
     public PersonType getPersonType() {
         return PersonType.STUDENT;
--- src/main/java/seedu/address/model/person/Tutor.java
+++ src/main/java/seedu/address/model/person/Tutor.java
@@ -4,6 +4,10 @@ import java.util.Set;
 
 import seedu.address.model.tag.Tag;
 
+/**
+ * Represents a Tutor in the address book.
+ * Guarantees: details are present and not null, field values are validated, immutable.
+ */
 public class Tutor extends Person {
     /**
      * Constructs a {@code Tutor}.
@@ -14,11 +18,15 @@ public class Tutor extends Person {
      * @param address A valid address.
      * @param tags A set of tags.
      */
-    public Tutor(Name name, Phone phone, Email email, Address address,
-                    Set<Tag> tags) {
+    public Tutor(Name name, Phone phone, Email email, Address address, Set<Tag> tags) {
         super(name, phone, email, address, tags);
     }
 
+    /**
+     * Returns the type of this person.
+     *
+     * @return The person type TUTOR.
+     */
     @Override
     public PersonType getPersonType() {
         return PersonType.TUTOR;
