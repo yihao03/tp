1ceef13a127095468e7db89f28dc79457e3e1f87
yihao <yihao039@gmail.com>
2025-10-13 20:54:30 +0800

    chore: Lint

--- src/main/java/seedu/address/logic/parser/AddClassCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddClassCommandParser.java
@@ -18,7 +18,7 @@ public class AddClassCommandParser implements Parser<AddClassCommand> {
     /**
      * Parses the given {@code String} of arguments in the context of the AddClassCommand
      * and returns an AddClassCommand object for execution.
-     * 
+     *
      * @throws ParseException if the user input does not conform the expected format
      */
     public AddClassCommand parse(String args) throws ParseException {
