cfd5bdc8c4eada881115c0cc2e210b8cad19434d
algebananazzzzz <algebananazzzzz.devops@gmail.com>
2025-10-28 18:24:04 +0800

    trigger workflow

--- src/main/java/seedu/address/Main.java
+++ src/main/java/seedu/address/Main.java
@@ -12,7 +12,6 @@ import seedu.address.commons.core.LogsCenter;
  * entry point of the application:
  *
  *     Error: JavaFX runtime components are missing, and are required to run this application
- *
  * The reason is that MainApp extends Application. In that case, the
  * LauncherHelper will check for the javafx.graphics module to be present
  * as a named module. We don't use JavaFX via the module system so it can't
